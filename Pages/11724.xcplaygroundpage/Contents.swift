71197c5369444d57af1005da0359d2a4f011383b
Jaewook Lim <esl112999@gmail.com>
2021-12-28 16:10:51 +0900

    **Problem 11724 Update 2021.12.28**

---
+++ Pages/11724.xcplaygroundpage/Contents.swift
@@ -0,0 +1,82 @@
+import Foundation
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+func solution() -> Int{
+    let fIO = FileIO()
+    let N = fIO.readInt()
+    let M = fIO.readInt()
+    var visited = [Int](repeating: 0, count: N+1)
+    for i in 1...N{
+        visited[i] = i
+    }
+    for _ in 0..<M{
+        let a = fIO.readInt()
+        let b = fIO.readInt()
+        let min = visited[a] < visited[b] ? visited[a] : visited[b]
+        let max = visited[a] > visited[b] ? visited[a] : visited[b]
+        if min == max { continue }
+        for i in 1...N{
+            if visited[i] == max { visited[i] = min }
+        }
+    }
+    visited.removeFirst()
+    
+    return Set(visited).count
+}
+
+print(solution())
---
+++ Pages/14442 try.xcplaygroundpage/Contents.swift
@@ -0,0 +1,158 @@
+import Foundation
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+struct DoubleStackQueue<Element> {
+    private var inbox: [Element] = []
+    private var outbox: [Element] = []
+    
+    var isEmpty: Bool{
+        return inbox.isEmpty && outbox.isEmpty
+    }
+    
+    var count: Int{
+        return inbox.count + outbox.count
+    }
+    
+    var front: Element? {
+        return outbox.last ?? inbox.first
+    }
+    
+    init() {
+    }
+    
+    init(_ array: [Element]) {
+        self.init()
+        self.inbox = array
+    }
+    
+    mutating func enqueue(_ n: Element) {
+        inbox.append(n)
+    }
+    
+    mutating func dequeue() -> Element {
+        if outbox.isEmpty {
+            outbox = inbox.reversed()
+            inbox.removeAll()
+        }
+        return outbox.removeLast()
+    }
+}
+
+extension DoubleStackQueue: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: Element...) {
+        self.init()
+        inbox = elements
+    }
+}
+
+struct Point: Hashable{
+    let x: Int
+    let y: Int
+    let cnt: Int
+    init(_ x: Int, _ y: Int, _ cnt: Int){
+        self.x = x
+        self.y = y
+        self.cnt = cnt
+    }
+}
+let dx = [1,0,-1,0]
+let dy = [0,1,0,-1]
+
+
+
+func solution() -> Int{
+    func isBound(_ xx: Int, _ yy: Int) -> Bool{
+        return xx >= 0 && yy >= 0 && xx < N && yy < M
+    }
+    let fIO = FileIO()
+    let N = fIO.readInt()
+    let M = fIO.readInt()
+    let K = fIO.readInt()
+    var visited = [[Int]](repeating: [Int](repeating: 11, count: M), count: N)
+    let start = Point(0, 0, 0)
+    visited[start.x][start.y] = 0
+    var board = [[Int]]()
+    for _ in 0..<N{
+        board.append(fIO.readString().map{Int(String($0))!})
+
+    }
+    var q = DoubleStackQueue<Point>()
+    q.enqueue(start)
+    var depth = 1
+    while !q.isEmpty{
+        for _ in 0..<q.count{
+            let a = q.dequeue()
+            for k in 0..<4{
+                let xx = a.x+dx[k]
+                let yy = a.y+dy[k]
+                if !isBound(xx, yy){ continue }
+                let cnt = board[xx][yy]+a.cnt
+                if cnt > K { continue }
+                if visited[xx][yy] < cnt { continue }
+                if xx == N-1 && yy == M-1 { return depth+1 }
+                visited[xx][yy] = cnt
+                q.enqueue(Point(xx, yy, cnt))
+            }
+        }
+        depth += 1
+    }
+    return -1
+}
+
+print(solution())
