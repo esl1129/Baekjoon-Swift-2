3cf5aa6893a4d1595cd5ea206825d3054a0dd4c0
Jaewook Lim <esl112999@gmail.com>
2021-12-29 10:56:17 +0900

    **Problem 20055 Update 2021.12.29**

---
+++ Pages/20055.xcplaygroundpage/Contents.swift
@@ -0,0 +1,132 @@
+import Foundation
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+struct DoubleStackQueue<Element> {
+    private var inbox: [Element] = []
+    private var outbox: [Element] = []
+    
+    var isEmpty: Bool{
+        return inbox.isEmpty && outbox.isEmpty
+    }
+    
+    var count: Int{
+        return inbox.count + outbox.count
+    }
+    
+    var front: Element? {
+        return outbox.last ?? inbox.first
+    }
+    
+    init() { }
+    
+    init(_ array: [Element]) {
+        self.init()
+        self.inbox = array
+    }
+    
+    mutating func enqueue(_ n: Element) {
+        inbox.append(n)
+    }
+    
+    mutating func dequeue() -> Element {
+        if outbox.isEmpty {
+            outbox = inbox.reversed()
+            inbox.removeAll()
+        }
+        return outbox.removeLast()
+    }
+}
+
+extension DoubleStackQueue: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: Element...) {
+        self.init()
+        inbox = elements
+    }
+}
+
+func solution() -> Int{
+    let fIO = FileIO()
+    let N = fIO.readInt()
+    let K = fIO.readInt()
+    var trailer = [Int]()
+    var robot = [Int](repeating: 0, count: N)
+    for _ in 0..<2*N{
+        trailer.append(fIO.readInt())
+    }
+    var answer = 1
+    while true{
+        trailer = [trailer.removeLast()]+trailer
+        robot = [robot.removeLast()]+robot
+        robot[N-1] = 0
+        for i in stride(from: N-2, to: 0, by: -1){
+            if robot[i] == 0 || trailer[i+1] <= 0 || robot[i+1] != 0 { continue }
+            robot[i+1] = 1
+            robot[i] = 0
+            trailer[i+1] -= 1
+        }
+        robot[N-1] = 0
+        if trailer[0] > 0 {
+            robot[0] = 1
+            trailer[0] -= 1
+        }
+        if trailer.filter{$0 == 0}.count >= K { return answer }
+        answer += 1
+    }
+}
+
+print(solution())
