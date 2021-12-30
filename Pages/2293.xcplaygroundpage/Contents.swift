6ff78653ceee98d58a55aa8f8dadf54bf6e1b09e
Jaewook Lim <esl112999@gmail.com>
2021-12-30 19:32:00 +0900

    **Problem 2293 Update 2021.12.30**

---
+++ Pages/2293.xcplaygroundpage/Contents.swift
@@ -0,0 +1,80 @@
+import Foundation
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+func solution() -> Int{
+    let MAX = 4294967296
+    let fIO = FileIO()
+    let N = fIO.readInt()
+    let K = fIO.readInt()
+    var dp = [Int](repeating: 0, count: K+1)
+    var token = [Int]()
+    dp[0] = 1
+    for _ in 0..<N{
+        token.append(fIO.readInt())
+    }
+    for t in token.sorted{$0 > $1}{
+        for i in 1...K{
+            if i-t < 0 { continue }
+            dp[i] += dp[i-t]
+            if dp[i] > MAX { dp[i] = 0}
+        }
+    }
+    return dp[K]
+}
+print(solution())
