e4885402e712cfe77df777b06b6e384c42e12976
Jaewook Lim <esl112999@gmail.com>
2022-01-03 19:54:08 +0900

    **Problem 1309 Update 2022.01.03**

---
+++ Pages/1309.xcplaygroundpage/Contents.swift
@@ -0,0 +1,81 @@
+import Foundation
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+struct cnt{
+    let no: Int
+    let le: Int
+    let ri: Int
+    func getSum() -> Int{
+        return no+le+ri
+    }
+}
+func solution() -> Int{
+    let fIO = FileIO()
+    let N = fIO.readInt()
+    var dp = [cnt(no: 1, le: 1, ri: 1)]
+    
+    for i in 0..<N-1{
+        let n = (dp[i].ri+dp[i].no+dp[i].le)%9901
+        let l = (dp[i].ri+dp[i].no)%9901
+        let r = (dp[i].no+dp[i].le)%9901
+        dp.append(cnt(no: n, le: l, ri: r))
+    }
+    return (dp[N-1].ri+dp[N-1].no+dp[N-1].le)%9901
+}
+print(solution())
