8a42b31fc7873d163966c0a96da3943b8f1a7a18
Jaewook Lim <esl112999@gmail.com>
2022-01-06 19:20:05 +0900

    **Problem 1146 Update 2022.01.06**

---
+++ Pages/1205.xcplaygroundpage/Contents.swift
@@ -0,0 +1,90 @@
+import Foundation
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer[index]
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+        
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+        
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+func solution() -> Int{
+    let fIO = FileIO()
+    let N = fIO.readInt()
+    let T = fIO.readInt()
+    let P = fIO.readInt()
+    var dict = [Int:Int]()
+    for _ in 0..<N{
+        let now = fIO.readInt()
+        if dict[now] == nil{
+            dict[now] = 1
+        } else {
+            dict[now]! += 1
+        }
+    }
+    var rank = 1
+    var cnt = 1
+    for (key, value) in dict.sorted{$0.key > $1.key}{
+        if T >= key{
+            if T == key{
+                cnt += value
+            }
+            break
+        }
+        if T < key {
+            rank += value
+            cnt += value
+            continue
+        }
+    }
+    return cnt <= P ? rank : -1
+}
+print(solution())
